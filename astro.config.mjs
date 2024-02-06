38fa2ecbe41c7043448282edaf3da5a68fe47b78
odinalx <odin.alexandre26@gmail.com>
2024-02-06 16:56:58 +0100

    fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,12 @@
 import { defineConfig } from 'astro/config';
 
 import svelte from "@astrojs/svelte";
-import vercel from '@astrojs/vercel/serverless';
+import vercel from '@astrojs/vercel/static';
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [svelte()],
-  output: 'server',
+  output: 'static',
   adapter: vercel({
     webAnalytics: {
       enabled: true,
